728acf12455f8782c104a834e682988ee16a0ffa
MauroJRamos <mauroramosti@gmail.com>
2021-12-22 22:46:13 -0300

    Fix: genre

--- Challenger_MovieFlix/src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ Challenger_MovieFlix/src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -17,6 +17,7 @@ import javax.persistence.Table;
 public class Genre implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
